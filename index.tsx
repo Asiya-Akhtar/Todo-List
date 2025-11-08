357ac3e52385a0b47825db51860a1ee09610b30d
dell <asiyaakhtar17@gmail.com>
2025-11-08 20:55:33 +0500

    Fix router basename for GitHub Pages

--- index.tsx
+++ index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 
 const rootElement = document.getElementById('root');
@@ -8,8 +9,11 @@ if (!rootElement) {
 }
 
 const root = ReactDOM.createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/Todo-List">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
